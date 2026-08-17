daa995f19476f80674ca82d2c4aec42539192d0b
Prathmesh9999 <prathmeshgholap999@gmail.com>
2026-08-17 13:41:14 +0530

    feat: add initial client & server .exe

---
+++ client/client_main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "DevHub Client v1.0" << std::endl;
+
+    return 0;
+}
---
+++ server/server_main.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+int main(){
+    std::cout<<"DevHub Server v1.0"<<std::endl;
+    return 0;
+}
